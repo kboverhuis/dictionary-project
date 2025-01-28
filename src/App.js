8f621d21ed7b0e7c86e32551e03930fbd12362bd
kboverhuis <kboverhuis@gmail.com>
2025-01-27 19:37:34 -0600

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './shecodes-logo.png';
 import './App.css';
+import Dictionary from './Dictionary'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      Hello World
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          Hello World
+        </header>
+        <main>
+          <Dictionary />
+          <footer className='App-footer'>
+            Coded by Kristi Boverhuis
+          </footer>
+        </main>
+      </div>
     </div>
   );
 }
