71c625f686d723aa078391247b4d75e3faef74aa
kboverhuis <kboverhuis@gmail.com>
2025-01-27 18:55:57 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './shecodes-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      Hello World
+      </header>
+    </div>
+  );
+}
+
+export default App;
