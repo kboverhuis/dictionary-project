4d1c0abff7acc07a07a58d90df68dca4df261e1e
kboverhuis <kboverhuis@gmail.com>
2025-02-03 21:32:17 -0600

    Added Synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms"
 import "./Dictionary.css";
 
 export default function Meaning(props) {
@@ -8,7 +9,8 @@ export default function Meaning(props) {
       <div className="Speech">
         <strong>{props.meaning.partOfSpeech}</strong>{" "}
       </div>
-      <div className="Definition">{props.meaning.definition}</div>
+      <div className="Definition"><strong>Definition: </strong>{props.meaning.definition}</div>
+      <Synonyms synonyms={props.meaning.synonyms}/>
     </div>
   );
 }
