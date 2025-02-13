f399c6f4b1ea32f953a1c056b00738754e6a5a67
kboverhuis <kboverhuis@gmail.com>
2025-02-12 22:47:57 -0600

    Updated Photo.js

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ if (props.photos) {
           console.log(photo);
           return (
             <div className="col-4" key={index}>
-              <a href={photo.src.original} target="_blank">
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
                 <img src={photo.src.landscape} className="img-fluid" />
               </a>
             </div>
